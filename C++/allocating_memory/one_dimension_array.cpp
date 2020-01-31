0fca7ef28d3db46229c8dee5d0984a364e50bf83
Long <phannguyenlong0812@gmail.com>
2020-01-31 20:16:57 +0700

    update: learn allocating mem in C++

---
+++ C++/allocating_memory/one_dimension_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // create 20 memory slot type int (4 bypte/each)
+    int * array = new int[20];
+    if ( !array ) {
+        cout << "Error when allocating memory" << endl;
+    } else {
+        for (int i= 0; i < 20; i++) {
+            array[i] = i;
+        }
+        for (int i = 0; i <20; i++) {
+            cout << array[i] << endl;
+        }
+    }
+    // delete memory
+    delete [] array;
+    for (int i=0; i<20; i++) {
+        cout << array[i] <<endl;
+    }
+}
