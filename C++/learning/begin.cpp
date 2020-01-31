632f996c079e5fbef99f27f9c95a432ca3674b69
Long <phannguyenlong0812@gmail.com>
2020-01-31 15:01:02 +0700

    update: update on C++ learning

--- C++/learning/begin.cpp
+++ C++/learning/begin.cpp
@@ -5,6 +5,78 @@
 using namespace std;
 void sayHello( int age = 10, string name="Some random name"); // set defaul parameter
 
+// class and object
+class Book {
+    public: 
+        string title;
+        string author;
+        int pages;
+        // this can have parameter in contructor function instead if this
+        Book() { // contructor funciton: will run when an object is create (MUST BE SAME NAME WITH CLASS)
+            cout << "Enter title: ";
+            getline(cin, title);
+            cout << "Enter author: ";
+            getline(cin, author);
+            cout << "Enter pages: ";
+            cin >> pages;
+        };
+        bool isLongBook() { // object function, will be call in main
+            return pages >= 100 ? true : false;
+        }
+};
+
+// getter and setter
+class Movie {
+    private: // những thuộc tính trong private thì chỉ có chỉnh bởi những function nằm trong class movie, những cái nằm ngoài không chỉnh được
+        string rating;
+    public:
+        string title;
+        string director;
+        Movie() { // create initial value for class if there are none of information
+            title= "no title";
+            director = "no director";
+            setRating("NG");
+        }
+        Movie( string aTitle, string aDirector, string aRating) {
+            title = aTitle;
+            director = aDirector;
+            setRating(aRating); // must use like this to controll the input
+        };
+        void setRating(string aRating) {
+            rating = aRating == "good" ? aRating : "NG"; // controll that the rating always set to be good :)))
+        };
+        string getRating() {
+            return rating;
+        }
+};
+
+// inherit
+class Chef{
+    public:
+        void makeChicken(){
+            cout << "The chef makes chicken" << endl;
+        }
+
+        void makeSalad(){
+            cout << "The chef makes salad" << endl;
+        }
+
+        void makeSpecialDish(){
+            cout << "The chef makes a special dish" << endl;
+        }
+};
+class ItalianChef : public Chef{ // thừa hưởng (inherit) các thuộc tính có sẵn của class chef (có thể thừa hưởng từ nhiều class, để ý thứ tự)
+    public:
+        void makePasta(){
+            cout << "The chef makes pasta" << endl;
+        }
+
+        // override: có thể ghi đè và thay đổi thuộc tính mà nó được thừa hưởng mà không ảnh hưởng tới class bị thừa hưởng
+        void makeSpecialDish(){
+            cout << "The chef makes chicken parm" << endl;
+        }
+};
+
 int main()
 {
     // basic print out
@@ -32,6 +104,29 @@ int main()
     // array y chang trong C
     // function giong C, C++ co them defaul parameter: được thêm ở prototype ở trên
     sayHello();
+
+    /*=======================BASIC OOP===============================*/
+
+    // classes (nằm ở trên đầu) and object trong C
+    Book book1; // define an object with datatype is book
+    // can modify content after create
+    book1.title = "Long tom";
+    book1.author = "the grate long tom";
+    book1.pages = 10;
+    // calling an object function declare before
+    cout << book1.isLongBook() << endl;// return true if pages > 100, this is call object function
+
+    // GETTER and SETTER
+    Movie movie1("Titanic", "Long Tom", "bab"); // tạo class movie
+    // movie1.rating = "shit" // will cause error cause rating is in private can not be modify here like public
+    cout << "Rating of the movie is: " <<movie1.getRating(); // must use the function to get Rating
+
+    // INHERIT (class define ở phía trên)
+    Chef myChef;
+    myChef.makeChicken();
+
+    ItalianChef myItalianChef;
+    myItalianChef.makeChicken();
 
     return 0;
 }
