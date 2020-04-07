60d4666446b4fca3a3705654cbb66fb20a74a562
Long <phannguyenlong0812@gmail.com>
2020-04-07 21:42:28 +0700

    exercise: update q3 with file operator

--- C++/excercise/ex2/q3_managerSystem.cpp
+++ C++/excercise/ex2/q3_managerSystem.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
 using namespace std;
 
 class Student {
@@ -17,6 +18,8 @@ class Student {
             gender = gender_list[gender_choice];
             cout << "New Student was created" << endl;
         }
+        Student (int id, string fname, string lname, string major, string gender, float GPA)
+        : id(id), fname(fname), lname(lname), major(major), gender(gender), GPA(GPA) {}
         void modify() {
             string major_list[4] = {"computer science","information technology","software engineering","information system"};
             string gender_list[2] = {"male", "female"};
@@ -66,13 +69,51 @@ class Student {
             os << "\nGender: " << std.gender << "\nMajor: " << std.major << "\nGPA: " << std.GPA;
             return os;
         }
+        friend void writeFile(const vector <Student *>&);
 };
+
+void readFile(vector <Student*>& list) {
+    ifstream fin; // file pointer
+    int id;
+    float GPA;
+    string address, lname, fname, major, gender;
+    cout << "Input link to your file: ";
+    cin >> address;
+    fin.open(address, ios::in); // use input mode
+    if (!fin) {
+        cout << "Unable to reach file";
+        exit(1); // break the program
+    } else {
+        while (!fin.eof()) {
+            fin >> id;
+            fin >> fname;
+            getline(fin, lname, '|');
+            getline(fin, gender, '|');
+            getline(fin, major, '|');
+            fin >> GPA;
+            list.push_back(new Student(id, fname, lname, major, gender, GPA));
+        }
+        fin.close();
+    }
+}
+void writeFile(const vector <Student*> &list) {
+    ofstream fout; // pointer to file output
+    string address;
+    cout << "Input link to saved file: ";
+    cin >> address;
+    fout.open(address, ios::out); // set to ouput mode
+    for (int i=0; i < list.size(); i++) {
+        fout <<  list[i]->id << " " << list[i]->lname << list[i]->fname << "|" ;
+        fout << list[i]->gender << "|" << list[i]->major << "|" << list[i]->GPA << "\n";
+    }
+}
 
 int main() {
     int choice =1, major, gender, id, pos;
     float gpa;
     string fname, lname;
     vector <Student*> student_system;
+    readFile(student_system);
     while (choice != 6) {
         cout << "1 - Add Record\n2 - List Record\n3 - Modify Record\n4 - Delete Record\n5 - Search Record\n6 - Exit\nInput choice: ";
         cin >> choice;
@@ -136,4 +177,5 @@ int main() {
                 break;
         }
     }
+    writeFile(student_system);
 }
