c31481f71933fc2f7889ac972d600ab28699e7c0
Long <phannguyenlong0812@gmail.com>
2020-05-12 10:25:13 +0700

    learn: bubble sort

---
+++ algorithsm_data/sorting/comparision_base_sort/elementary_sort/bubble_sort.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int *inputData(int size) {
+    int *arr = new int[size];
+    for (int i=0; i < size; i++) {
+        arr[i] = rand() % 20;
+    }
+    return arr;
+}
+void outputData(int *arr, int size) {
+    for (int i=0; i < size; i++) {
+        cout << arr[i] << "\t";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = i + 1; j < size; j++) {
+            if (arr[i] > arr[j]) { // this is basic operation
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int size;
+    srand(1);
+
+    cout << "Enter array size: ";
+    cin >> size;
+    int *arr = new int[size];
+
+    arr = inputData(size);
+    cout << "Before: ";
+    outputData(arr, size);
+
+    bubbleSort(arr, size);
+    cout << "After: ";
+    outputData(arr, size);
+}
