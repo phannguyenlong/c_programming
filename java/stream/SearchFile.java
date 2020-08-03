f34ca3bd26841e95f930587258f1d1e088e75c74
Long <phannguyenlong0812@gmail.com>
2020-08-03 21:21:00 +0700

    learn: update search many type file instead of pdf only

--- java/stream/SearchFile.java
+++ java/stream/SearchFile.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
@@ -9,14 +10,25 @@ import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Scanner;
 
+/**
+ * Search for pdf file
+ */
 public class SearchFile extends SimpleFileVisitor<Path> {
-    private static final PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:*.pdf");
+    private static PathMatcher matcher;
 
+    public SearchFile(String matcher) {
+        SearchFile.matcher = FileSystems.getDefault().getPathMatcher(matcher); // ex: glob:*pdf
+    }
+
+    public static void setMatcher(String matcher) {
+        SearchFile.matcher = FileSystems.getDefault().getPathMatcher(matcher); // ex: glob:*pdf
+    }
+    
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if (attrs.isRegularFile()) {
             if (matcher.matches(file.getFileName())) {
-                System.out.println("Match: " + file.getFileName());
+                System.out.println("Match: " + file);
             }
         }
         return FileVisitResult.CONTINUE;
@@ -27,10 +39,17 @@ public class SearchFile extends SimpleFileVisitor<Path> {
             System.out.print("Input path to search: ");
             String p = scan.nextLine();
             Path path = Paths.get(p);
-            
-            SearchFile search = new SearchFile();
+
+            System.out.print("Input syntax to search (ex: glob:*.pdf): ");
+            String matcher = scan.nextLine();
+
+            SearchFile search = new SearchFile(matcher);
             Files.walkFileTree(path, search);
 
+        } catch (InvalidPathException e) {
+            System.out.println("Invalid Path Syntaxt");
+        } catch (UnsupportedOperationException | IllegalArgumentException e) {
+            System.out.println("Wrong syntaxt: " + e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
         }
